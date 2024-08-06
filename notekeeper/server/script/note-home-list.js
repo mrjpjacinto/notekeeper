db736336595ae355cbfa477fa00b8d73302c74f7
fymal1229 <fayeyunicemarielaxamana@gmail.com>
2024-08-06 16:26:25 +0800

    Added dropdown menu

--- notekeeper/server/script/note-home-list.js
+++ notekeeper/server/script/note-home-list.js
@@ -2,3 +2,19 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+ function toggleMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
