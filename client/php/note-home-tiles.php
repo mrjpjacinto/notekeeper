6928f1e2375cd254a26945590ae9fc8d25717184
younglingslayer9000 <khylled.0727@gmail.com>
2024-08-07 14:19:21 +0800

    fixed home tiles path

--- client/php/note-home-tiles.php
+++ client/php/note-home-tiles.php
@@ -1,3 +1,12 @@
+<?php
+ session_start();
+
+ if(!isset($_SESSION['uname'])) {
+    header("Location: /notekeeper/client/php/note-login.php");
+
+    exit();
+ }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
