0be4ec57c0f75ade6c08e2b7f6a2215d97819015
younglingslayer9000 <khylled.0727@gmail.com>
2024-08-07 11:00:59 +0800

    added timer

--- server/script/note-login.js
+++ server/script/note-login.js
@@ -7,3 +7,18 @@ function showPassword() {
     }
   }
 
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const errorMsg = document.getElementById('error-msg');
+    if (errorMsg) {
+        setTimeout(() => {
+            errorMsg.style.display = 'none';
+        }, 2000);
+    }
+    const successMsg = document.getElementById('success-msg');
+    if (successMsg) {
+      setTimeout(() => {
+        successMsg.style.display = 'none';
+        window.location.href = "/notekeeper/client/php/note-home-list.php";
+      }, 2000);
+    }
+})
