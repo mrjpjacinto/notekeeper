7f4361640202731dbb17c58df27717316d4f70f5
younglingslayer9000 <khylled.0727@gmail.com>
2024-08-14 17:44:14 +0800

    new database

--- database/notepad_db.sql
+++ database/notepad_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 13, 2024 at 12:12 PM
+-- Generation Time: Aug 14, 2024 at 11:43 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,8 +41,12 @@ CREATE TABLE `notes` (
 --
 
 INSERT INTO `notes` (`id`, `title`, `content`, `color`, `date_created`, `user_id`) VALUES
-(53, 'khylle', 'gaboaiwoidbwdbawdbadadvawudv', 'default-color', '2024-08-13 17:52:07', 14),
-(54, 'afdsdsa', 'asddasdsdassaddas', 'default-color', '2024-08-13 18:06:57', 14);
+(63, 'A Song of Ice and Fire', 'the besttttttttt', 'default-color', '2024-08-14 14:01:21', 14),
+(64, 'khylle', 'gabaawrrwawrra', 'default-color', '2024-08-14 14:10:53', 14),
+(65, 'fefefef', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaffsdbjksdfbskjdfbsjlvfsehf', 'default-color', '2024-08-14 14:30:21', 14),
+(68, 'lorem ipsum', 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww\r\nwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww\r\nwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', 'default-color', '2024-08-14 14:40:44', 14),
+(69, 'song', 'vvvvxcvcxvxcccccccccccccccccccccccccccccc', 'default-color', '2024-08-14 14:46:03', 14),
+(70, 'zxc', 'zxc', 'default-color', '2024-08-14 17:14:34', 14);
 
 -- --------------------------------------------------------
 
@@ -67,7 +71,9 @@ INSERT INTO `user` (`id`, `email`, `passwd`, `fname`, `lname`, `uname`) VALUES
 (14, 'khylle@gmail.com', '$2y$10$k3ji23HCnqy404SOmTHtRuyp0/C6qQcSszH/vztErJIYeUxur5b66', 'Khylle', 'Diño', 'khylle0'),
 (15, 'sasuke@gmail.com', '$2y$10$v.G13NEnHE.FMl0vfw0u7u33iIhKlqhMhhbNeVRV/fd/8wmpetvKi', 'sasuke', 'sasuke', 'sasuke'),
 (16, 'tope@gmail.com', '$2y$10$Hz4t9yRAGvQhf3xkJiV/N.vSlS65EAExasGNrvCFsiLvuhSz2dDBe', 'tope', 'tope', 'tope'),
-(17, 'tope@gmail.com', '$2y$10$bo43ibLhlNvz2UUV0Db7/ey0YM1kLldtr9Pz2KRkzu8xtd1jbvdNa', 'tope', 'tope', 'tope');
+(18, 'fafo@gmail.com', '$2y$10$Rv7N19zNYhoGnaQ8KZhFzeSI6iORb7gZYW.rola3M80wI06JxZhPW', 'fafo', 'fafo', 'fafo'),
+(19, 'faye@gmail.com', '$2y$10$.CAUQgMLd/bCQ45DVTuioeEBFQKfm2Mdb3tTgrjMbQkDicNnoIPhy', 'faye', 'faye', 'faye'),
+(20, 'khylled.0727@gmail.com', '$2y$10$kV5vgJCBw5G5EKYlP8v7JOmdex9.rehHnQH5wNOyz7c6x/be.1PJ2', 'Khylle', 'khylle', 'khylleeeeeee');
 
 --
 -- Indexes for dumped tables
@@ -84,7 +90,9 @@ ALTER TABLE `notes`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `uname` (`uname`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -94,13 +102,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
