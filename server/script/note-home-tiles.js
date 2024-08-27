f8bd1d5775bda114bfb0a290966f2806ff574741
younglingslayer9000 <khylled.0727@gmail.com>
2024-08-27 14:24:29 +0800

    removed not working functions

--- server/script/note-home-tiles.js
+++ server/script/note-home-tiles.js
@@ -230,14 +230,6 @@ function cancelSelect() {
     updateSelectedCount();
 }
 
-function sortFavorites() {
-  const navFavorites = document.getElementById('navFavorites');
-  if (navFavorites.style.display === 'flex') {
-    navFavorites.style.display = 'none';
-  } else {
-    navFavorites.style.display = 'flex';
-  }
-}
 
 // Toggle note selection
 function toggleSelection(event) {
